3cb3c8a7da77382725c4e0f682f7a26cbe73bc95
dianmianqizi <115695454+dianmianqizi@users.noreply.github.com>
2023-12-20 11:14:52 +0800

    Update syn.sh

--- syn.sh
+++ syn.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 curl https://gist.githubusercontent.com/ddgksf2013/dbb1695cd96743eef18f3fac5c6fe227/raw/revenuecat.js > revenuecat.js
+curl https://gist.githubusercontent.com/ddgksf2013/beec132ca0c3570ffa0cf331bce8f82a/raw/baidumap.adblock.conf > baidumap.adblock.conf
