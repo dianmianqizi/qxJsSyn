2fa254094a7d1cfb8144b70f96c9fb7adc5c51b6
dianmianqizi <dianmianqizi@qq.com>
2023-12-20 03:05:26 +0000

    action:Wed Dec 20 03:05:26 UTC 2023

---
+++ revenuecat.js
@@ -0,0 +1,260 @@
+/***********************************
+
+> ScriptName        RevenueCat多合一脚本[墨鱼版]
+> Author            @ddgksf2013
+> ForHelp           若有屏蔽广告的需求，可公众号后台回复APP名称
+> WechatID          墨鱼手记
+> TgChannel         https://t.me/ddgksf2021
+> Contribute        https://t.me/ddgksf2013_bot
+> Feedback          📮 ddgksf2013@163.com 📮
+> UpdateTime        2023-12-20
+> Suitable          自行观看“# > ”注释内容，解锁是暂时的，购买也不是永久的[订阅、跑路]
+> Attention         如需引用请注明出处，谢谢合作！
+> ScriptURL         https://gist.githubusercontent.com/ddgksf2013/dbb1695cd96743eef18f3fac5c6fe227/raw/revenuecat.js
+
+# ========解锁列表======== #
+# > 01 白云天气
+https://apps.apple.com/cn/app/id1575901953
+# > 02 1Blocker
+https://apps.apple.com/cn/app/id1365531024
+# > 03 Aphrodite
+https://apps.apple.com/cn/app/id1568289454
+# > 04 Apollo
+https://apps.apple.com/cn/app/id1616467801
+# > 05 pandora
+https://apps.apple.com/cn/app/id1470560916
+# > 06 widgetart
+https://apps.apple.com/cn/app/id1539097448
+# > 07 Spark
+https://apps.apple.com/cn/app/id997102246
+# > 08 Pillow
+https://apps.apple.com/cn/app/id878691772
+# > 09 图图记账
+https://apps.apple.com/cn/app/id1546356856
+# > 10 VSCO
+https://apps.apple.com/cn/app/id588013838
+# > 11 谜底时钟
+https://apps.apple.com/cn/app/id1536358464
+# > 12 谜底黑胶
+https://apps.apple.com/cn/app/id1606306441
+# > 13 OffScreen
+https://apps.apple.com/cn/app/id1474340105
+# > 14 花样文字
+https://apps.apple.com/cn/app/id1438854446
+# > 15 ScannerPro
+https://apps.apple.com/cn/app/id333710667
+# > 16 车票票
+https://apps.apple.com/cn/app/id6446212291
+# > 17 HTTPBot
+https://apps.apple.com/us/app/id1232603544
+# > 18 Audiomack
+https://apps.apple.com/cn/app/id921765888
+# > 19 ServerBee
+https://apps.apple.com/cn/app/id6443553714
+# > 20 NotBoring天气
+https://apps.apple.com/cn/app/id1531063436
+# > 21 NotBoring习惯
+https://apps.apple.com/cn/app/id1593891243
+# > 22 NotBoring计算器
+https://apps.apple.com/cn/app/id1533591596
+# > 23 NotBoring计时器
+https://apps.apple.com/cn/app/id1531048091
+# > 24 NotBoringVibes
+https://apps.apple.com/cn/app/id1661440185
+# > 25 倒数鸭
+https://apps.apple.com/cn/app/id6457201223
+# > 26 iptv-ultra
+https://apps.apple.com/cn/app/id1549657742
+# > 27 happy-days
+https://apps.apple.com/cn/app/id1564858029
+# > 28 chatai[非国区旧版V3.6]
+https://apps.apple.com/us/app/id1661016696
+# > 29 aptv[旧版V1.25]
+https://apps.apple.com/us/app/id1630403500
+# > 30 TouchRetouch
+https://apps.apple.com/cn/app/id373311252
+# > 31 方弗相机
+https://apps.apple.com/cn/app/id1621425556
+# > 32 Myjumplab
+https://apps.apple.com/us/app/id1554077178
+# > 33 目标地图
+https://apps.apple.com/cn/app/id1555022550
+# > 34 Paku
+https://apps.apple.com/cn/app/id1534130193
+# > 35 AwesomeHabits
+https://apps.apple.com/cn/app/id1514915737
+# > 36 Gear
+https://apps.apple.com/cn/app/id1458962238
+# > 37 MoneyThings
+https://apps.apple.com/cn/app/id1549694221
+# > 38 Anybox
+https://apps.apple.com/us/app/id1593408455
+# > 39 noto
+https://apps.apple.com/us/app/id1459055246
+# > 40 Widgetsmith
+https://apps.apple.com/cn/app/id1523682319
+# > 41 Percento
+https://apps.apple.com/cn/app/id1494319934
+# > 42 Planny
+https://apps.apple.com/cn/app/id1515324201
+# > 43 loopsie
+https://apps.apple.com/us/app/id1259909228
+# > 44 手机硬件管家
+https://apps.apple.com/cn/app/id1329937809
+# > 45 ImageX
+https://apps.apple.com/us/app/id1668530080
+# > 46 我的时间
+https://apps.apple.com/cn/app/id1481796842
+# > 47 Fin
+https://apps.apple.com/cn/app/id1489698531
+# > 48 星垂日记
+https://apps.apple.com/cn/app/id1663588935
+# > 49 星垂专注
+https://apps.apple.com/cn/app/id6446450915
+# > 50 Locket
+https://apps.apple.com/cn/app/id1600525061
+# > 51 one4wall
+https://apps.apple.com/us/app/id6446678464
+# > 52 mizframa
+https://apps.apple.com/cn/app/id6444951894
+# > 53 极简时钟
+https://apps.apple.com/cn/app/id1265404088
+# > 54 极简日记
+https://apps.apple.com/cn/app/id1568936702
+# > 55 治愈时钟
+https://apps.apple.com/cn/app/id1599856748
+# > 56 photomator
+https://apps.apple.com/cn/app/id1444636541
+# > 57 奇妙组件
+https://apps.apple.com/cn/app/id1466785009
+# > 58 structured
+https://apps.apple.com/cn/app/id1499198946
+# > 59 卡片馆
+https://apps.apple.com/cn/app/id1441120440
+# > 60 ColorWidgets
+https://apps.apple.com/cn/app/id1531594277
+# > 61 pdfviewer
+https://apps.apple.com/cn/app/id1120099014
+# > 62 vision
+https://apps.apple.com/us/app/id1572457968
+# > 63 photoroom
+https://apps.apple.com/us/app/id1455009060
+# > 64 pure记账
+https://apps.apple.com/cn/app/id6447791661
+# > 65 clica
+https://apps.apple.com/us/app/id1571012264
+# > 66 usage
+https://apps.apple.com/us/app/id970353453
+# > 67 TimeFinder
+https://apps.apple.com/cn/app/id1347447233
+# > 68 Brass
+https://apps.apple.com/cn/app/id1533158013
+# > 69 ElementNote
+https://apps.apple.com/cn/app/id6444015673
+# > 70 小决定
+https://apps.apple.com/cn/app/id1338769645
+# > 71 Tangerine
+https://apps.apple.com/cn/app/id1468882685
+# > 72 pastepal
+https://apps.apple.com/us/app/id1503446680
+# > 73 Airmail
+https://apps.apple.com/cn/app/id993160329
+# > 74 stresswatch
+https://apps.apple.com/us/app/id6444737095
+# > 75 IN paper
+https://apps.apple.com/cn/app/id1560313343
+# > 76 LemoFM
+https://apps.apple.com/cn/app/id6444756219
+# > 77 我的物品
+https://apps.apple.com/us/app/id1423132930
+
+
+
+[rewrite_local]
+
+# ～ RevenueCat@ddgksf2013
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-echo-response https://gist.githubusercontent.com/ddgksf2013/dbb1695cd96743eef18f3fac5c6fe227/raw/revenuecat.js
+^https:\/\/api\.revenuecat\.com\/.+\/subscribers\/[^/]+/(offerings|attributes)$ url request-header (\r\n)X-RevenueCat-ETag:.+(\r\n) request-header $1X-RevenueCat-ETag:$2
+
+[mitm]
+
+hostname=api.revenuecat.com
+
+***********************************/
+
+
+
+
+// ========= 动态ID ========= //
+const mapping = {
+  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
+  'LUTCamera': ['ProVersion', 'com.uzero.funforcam.monthlysub'],
+  'totowallet': ['all', 'com.ziheng.totowallet.yearly'],
+  'Aphrodite': ['all'],
+  'apollo': ['all'],
+  'widget_art': ['all'],
+  'OneBox': ['all'],
+  'Spark': ['premium'],
+  'Pillow': ['premium'],
+  '1Blocker': ['premium'],
+  'VSCO': ['membership'],
+  'UTC': ['Entitlement.Pro'],
+  '%E8%AC%8E%E5%BA%95%E9%BB%91%E8%86%A0': ['Entitlement.Pro'],
+  '%E8%AC%8E%E5%BA%95%E6%99%82%E9%90%98': ['Entitlement.Pro'],
+  'OffScreen': ['Entitlement.Pro'],
+  'ScannerPro': ['plus'],
+  'WhiteCloud': ['allaccess'],
+  'HTTPBot': ['pro'],
+  'audiomack': ['Premium1'],
+  'server_bee': ['Pro'],
+  'simple-': ['patron'],
+  'streaks': ['patron'],
+  'andyworks-calculator': ['patron'],
+  'vibes': ['patron'],
+  'CountDuck': ['premium', 'Lifetime'],
+  'IPTVUltra': ['premium'],
+  'Happy%3ADays': ['pro', 'happy_999_lifetime'],
+  'ChatGPTApp': ['Advanced'],
+  'APTV': ['pro'],
+  'TouchRetouchBasic': ['premium'],
+  'My%20Jump%20Lab': ['lifetime'],
+  '%E7%9B%AE%E6%A0%87%E5%9C%B0%E5%9B%BE': ['pro'],
+  'Paku': ['pro'],
+  'Awesome%20Habits': ['premium'],
+  'Gear': ['pro', 'com.gear.app.yearly'],
+  'MoneyThings': ['Premium'],
+  'Anybox': ['pro'],
+  'Fileball': ['filebox_pro'],
+  'Noto': ['pro'],
+  'Grow': ['grow.pro', 'grow_lifetime'],
+  'WidgetSmith': ['Premium'],
+  'Percento': ['premium'],
+  'Planny': ['premium'],
+  'CPUMonitor': ['Pro'],
+  'Locket': ['Gold'],
+  'My%20Tim': ['Pro'],
+  'Photom': ['premium', 'pixelmator_photo_pro_subscription_v1_pro_offer'],
+  'mizframa': ['premium', 'mf_20_lifetime2'],
+  'ImageX': ['imagex.pro.ios', 'imagex.pro.ios.lifetime'],
+  'Fin': ['premium', 'com.circles.fin.premium.yearly'],
+  'Ledger': ['Pro', 'com.lifetime.pro'],
+  'One4Wall': ['lifetime', 'lifetime_key'],
+  'OneWidget': ['allaccess'],
+  'CardPhoto': ['premium'],
+  'PDF%20Viewer': ['sub.pro'],
+  'PhotoRoom': ['business'],
+  'Decision': ['com.nixwang.decision.entitlements.pro'],
+  'Tangerine': ['Premium'],
+  'PastePal': ['premium'],
+  'Airmail': ['Airmail Premium'],
+  'Stress': ['StressWatch Pro'],
+  'PinPaper': ['allaccess'],
+  'Currency': ['plus'],
+  'Echo': ['PLUS'],
+  'MyThings': ['pro','xyz.jiaolong.MyThings.pro.infinity'],
+  'Overdue': ['Pro']
+};
+
+// =========    固定部分  ========= // 
+// =========  @ddgksf2021 ========= // 
+var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],cuttlefish={Attention:"恭喜你抓到元数据！由墨鱼分享，请勿售卖或分享他人！",request_date_ms:1662599120248,request_date:"2022-09-08T01:05:20Z",subscriber:{non_subscriptions:{},first_seen:"2022-09-08T01:04:03Z",original_application_version:"8",other_purchases:{},management_url:"https://apps.apple.com/account/subscriptions",subscriptions:{},entitlements:{},original_purchase_date:"2022-09-07T13:05:43Z",original_app_user_id:"$RCAnonymousID:ddgksf2013",last_seen:"2022-09-08T01:04:03Z"}},ddgksf2013={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"2099-12-18T01:04:17Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2022-09-08T01:04:18Z",purchase_date:"2022-09-08T01:04:17Z",store:"app_store"},ddgksf2021={grace_period_expires_date:null,purchase_date:"2022-09-08T01:04:17Z",product_identifier:"ddgksf2013_1y_128",expires_date:"2099-12-18T01:04:17Z"},obj=JSON.parse(JSON.stringify(cuttlefish));ddgksf2021.product_identifier="com.ddgksf2013.premium.yearly",obj.subscriber.subscriptions["com.ddgksf2013.premium.yearly"]=ddgksf2013;const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s&&(ddgksf2021.product_identifier=s,obj.subscriber.subscriptions[s]=ddgksf2013),obj.subscriber.entitlements[e]=ddgksf2021}else obj.subscriber.entitlements.pro=ddgksf2021;$done({body:JSON.stringify(obj)});
