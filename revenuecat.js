40ccc3ff8c9b9d697d1daabb54619f02bef7db09
dianmianqizi <dianmianqizi@qq.com>
2024-01-30 21:09:38 +0000

    action:Tue Jan 30 21:09:38 UTC 2024

--- revenuecat.js
+++ revenuecat.js
@@ -7,7 +7,7 @@
 > TgChannel         https://t.me/ddgksf2021
 > Contribute        https://t.me/ddgksf2013_bot
 > Feedback          📮 ddgksf2013@163.com 📮
-> UpdateTime        2024-01-28
+> UpdateTime        2024-01-30
 > Suitable          自行观看“# > ”注释内容，解锁是暂时的，购买也不是永久的[订阅、跑路]
 > Attention         如需引用请注明出处，谢谢合作！
 > ScriptURL         https://gist.githubusercontent.com/ddgksf2013/dbb1695cd96743eef18f3fac5c6fe227/raw/revenuecat.js
@@ -257,6 +257,8 @@ https://apps.apple.com/cn/app/id1548277133
 https://apps.apple.com/cn/app/id953286746
 # > 122 stoic
 https://apps.apple.com/cn/app//id1312926037
+# > 123 mojo
+https://apps.apple.com/cn/app/id1434861974
 
 
 [rewrite_local]
