6addcd1b158557cb62970272300e49ea7a555f56
dianmianqizi <dianmianqizi@qq.com>
2024-03-19 21:09:10 +0000

    action:Tue Mar 19 21:09:10 UTC 2024

--- revenuecat.js
+++ revenuecat.js
@@ -7,7 +7,7 @@
 > TgChannel         https://t.me/ddgksf2021
 > Contribute        https://t.me/ddgksf2013_bot
 > Feedback          📮 𝐝𝐝𝐠𝐤𝐬𝐟𝟐𝟎𝟏𝟑@𝟏𝟔𝟑.𝐜𝐨𝐦 📮
-> UpdateTime        2024-02-12
+> UpdateTime        2024-02-13
 > Suitable          自行观看“# > ”注释内容，解锁是暂时的，购买也不是永久的[订阅、跑路]
 > Attention         📣个别失效的APP请相关需求者自行降级、或寻找替代品、或购买支持
 > Attention         如需引用请注明出处，谢谢合作！
@@ -150,6 +150,7 @@ const mapping = {
   'card/': ['vip'],
   'EraseIt/': ['ProVersionLifeTime'],
   'Alpenglow': ['newPro'],
+  'MindBreathYoga/': ['lifetimeusa'],
   'MetadataEditor': ['unlimited_access'],
   '%E6%9F%A5%E5%A6%86%E5%A6%86': ['Pro access'],
   '%E5%85%83%E6%B0%94%E8%AE%A1%E6%97%B6': ['plus'],
