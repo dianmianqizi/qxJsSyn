e62694abab20bd7bfc994b04a4ee5640a77a7d03
dianmianqizi <dianmianqizi@qq.com>
2024-06-01 21:10:44 +0000

    action:Sat Jun  1 21:10:44 UTC 2024

--- revenuecat.js
+++ revenuecat.js
@@ -37,6 +37,7 @@ const mapping = {
   '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
   'FinancialNote': ['category'],
   'QingLong': ['Premium'],
+  'CircleTime/': ['Premium'],
   'ScreenRecordCase/': ['Premium'],
   'Currency-Converter/': ['pro'],
   'Precious/': ['Pro'],
