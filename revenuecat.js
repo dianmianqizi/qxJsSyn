b8a0a75dfc0e2ce6cceb0dc7c6e1204d6a120c32
dianmianqizi <dianmianqizi@qq.com>
2024-07-07 21:11:24 +0000

    action:Sun Jul  7 21:11:24 UTC 2024

--- revenuecat.js
+++ revenuecat.js
@@ -7,7 +7,7 @@
 > TgChannel         https://t.me/ddgksf2021
 > Contribute        https://t.me/ddgksf2013_bot
 > Feedback          📮 𝐝𝐝𝐠𝐤𝐬𝐟𝟐𝟎𝟏𝟑@𝟏𝟔𝟑.𝐜𝐨𝐦 📮
-> UpdateTime        2024-02-21
+> UpdateTime        2024-07-07
 > Suitable          自行观看“# > ”注释内容，解锁是暂时的，购买也不是永久的[订阅、跑路]
 > Attention         📣个别失效的APP请相关需求者自行降级、或寻找替代品、或购买支持
 > Attention         如需引用请注明出处，谢谢合作！
@@ -65,6 +65,7 @@ const mapping = {
   'Taskbit/': ['Pro'],
   'Spark': ['premium'],
   'Pillow': ['premium'],
+  'DecibelMeter/': ['Premium'],
   '1Blocker': ['premium'],
   'VSCO': ['membership'],
   'UTC': ['Entitlement.Pro'],
