2cd4c3910cb57819400aa5b91decda3f27117267
dianmianqizi <dianmianqizi@qq.com>
2024-08-24 21:10:06 +0000

    action:Sat Aug 24 21:10:06 UTC 2024

--- revenuecat.js
+++ revenuecat.js
@@ -7,7 +7,7 @@
 > TgChannel         https://t.me/ddgksf2021
 > Contribute        https://t.me/ddgksf2013_bot
 > Feedback          📮 𝐝𝐝𝐠𝐤𝐬𝐟𝟐𝟎𝟏𝟑@𝟏𝟔𝟑.𝐜𝐨𝐦 📮
-> UpdateTime        2024-07-05
+> UpdateTime        2024-07-06
 > Suitable          自行观看“# > ”注释内容，解锁是暂时的，购买也不是永久的[订阅、跑路]
 > Attention         📣个别失效的APP请相关需求者自行降级、或寻找替代品、或购买支持
 > Attention         如需引用请注明出处，谢谢合作！
@@ -45,6 +45,7 @@ const mapping = {
   'Precious/': ['Pro'],
   'Unfold/': ['PRO'],
   'mark_cup/': ['premiun'],
+  'StepUp/': ['premiun'],
   'SleepMaster/': ['premium','sm_14999_lifetime'],
   'Grain/': ['gold','lifetimeMembership'],
   'AudioPlayer': ['Pro'],
