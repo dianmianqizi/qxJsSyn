6e93d1f280fbe83381f094c3655c1dddbe38a79f
dianmianqizi <dianmianqizi@qq.com>
2024-10-01 21:12:52 +0000

    action:Tue Oct  1 21:12:52 UTC 2024

--- revenuecat.js
+++ revenuecat.js
@@ -7,7 +7,7 @@
 > TgChannel         https://t.me/ddgksf2021
 > Contribute        https://t.me/ddgksf2013_bot
 > Feedback          📮 𝐝𝐝𝐠𝐤𝐬𝐟𝟐𝟎𝟏𝟑@𝟏𝟔𝟑.𝐜𝐨𝐦 📮
-> UpdateTime        2024-07-07
+> UpdateTime        2024-07-08
 > Suitable          自行观看“# > ”注释内容，解锁是暂时的，购买也不是永久的[订阅、跑路]
 > Attention         📣个别失效的APP请相关需求者自行降级、或寻找替代品、或购买支持
 > Attention         如需引用请注明出处，谢谢合作！
@@ -80,7 +80,7 @@ const mapping = {
   'ScannerPro': ['plus'],
   'Duplete/': ['Pro'],
   'Ooga/': ['Ooga'],
-  'WhiteCloud': ['allaccess'],
+  'WhiteCloud': ['allaccess','wc_pro_1y'],
   'HTTPBot': ['pro'],
   'audiomack': ['Premium1'],
   'server_bee': ['Pro'],
